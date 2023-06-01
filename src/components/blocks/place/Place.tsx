7fbf77d6fa355483064c90a69d1947e34eb5633a
wrongangel <wrongangel@gmail.com>
2023-06-01 16:22:30 +0300

    place block

--- src/components/blocks/place/Place.tsx
+++ src/components/blocks/place/Place.tsx
@@ -1,7 +1,25 @@
+import Container from '@/components/elements/container/Container'
+import Image from 'next/image'
+import styles from './Place.module.scss'
+import placeImage from '@/assets/images/place.png'
+import Button from '@/components/elements/button/Button'
+
 interface Props {}
 const Place = (props: Props) => {
   return (
-    <div>Place</div>
+    <Container className={styles.place__container}>
+      <div className={styles.place__interactive}>
+        <Image src={placeImage} alt='' className={styles.place__interactive_image} />
+      </div>
+      <div className={styles.place__content}>
+        <h2 className={styles.place__content_header}>Find your <span className={styles.place__content_accent}>place</span></h2>
+        <div className={styles.place__content_text}>
+          <p>Name your budget and location and choose the right offer.</p>
+          <p>Get a match with perfect room or join forces with others and rent a whole flat together.</p>
+        </div>
+        <Button />
+      </div>
+    </Container>
   )
 }
 export default Place
