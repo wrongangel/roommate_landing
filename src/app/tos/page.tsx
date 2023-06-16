2ba3ccc72ab56e9845388a5c9a4ff3c0dfe77119
wrongangel <wrongangel@gmail.com>
2023-06-16 15:41:15 +0300

    Generated ToS

--- src/app/tos/page.tsx
+++ src/app/tos/page.tsx
@@ -1,13 +1,14 @@
 import Footer from '@/components/blocks/footer/Footer'
 import Header from '@/components/blocks/header/Header'
+import TOS from '@/components/blocks/tos/tos'
 import styles from '../page.module.scss'
 
-interface Props {}
+interface Props { }
 const ToS = (props: Props) => {
   return (
     <main className={styles.main}>
       <Header />
-      
+        <TOS />
       <Footer />
     </main>
   )
