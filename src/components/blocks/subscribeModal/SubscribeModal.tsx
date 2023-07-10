d81d1c5bc4401b2f8d88441ad2bea277e022e927
wrongangel <wrongangel@gmail.com>
2023-07-10 12:31:42 +0300

    RM-133 Close modal button

--- src/components/blocks/subscribeModal/SubscribeModal.tsx
+++ src/components/blocks/subscribeModal/SubscribeModal.tsx
@@ -4,6 +4,7 @@ import Container from '@/components/elements/container/Container'
 import Image from 'next/image'
 import styles from './SubscribeModal.module.scss'
 import logo from '@/assets/svg/logo.svg'
+import close from '@/assets/svg/closeWhite.svg'
 import SubscribeForm from '../subscribeForm/SubscribeForm'
 import { useTranslations } from 'next-intl'
 
@@ -17,6 +18,9 @@ const SubscribeModal = (props: Props) => {
     <div className={`${styles.modal} ${!props.open && styles.modal__hide}`} onClick={props.handleClose}>
       <Container className={styles.modal__container}>
         <div className={styles.modal__window} onClick={e => e.stopPropagation()}>
+          <button onClick={props.handleClose} className={styles.modal__closeButton}>
+            <Image src={close} alt='Close modal' width={32} height={32} />
+          </button>
           <div className={styles.modal__heroBlock}>
             <Image src={logo} alt='Roommate logo'  className={styles.modal__logo_image}/>
             <h2 className={styles.modal__logo_text}>Roommate.host</h2>
