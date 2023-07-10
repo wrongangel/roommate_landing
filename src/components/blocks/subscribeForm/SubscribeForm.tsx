8bc2bd43964c5c6df83639b542c2d67f01f21411
wrongangel <wrongangel@gmail.com>
2023-07-10 13:43:27 +0300

    RM-120 Thank you for subscribing

--- src/components/blocks/subscribeForm/SubscribeForm.tsx
+++ src/components/blocks/subscribeForm/SubscribeForm.tsx
@@ -4,7 +4,10 @@ import { useTranslations } from 'next-intl'
 import styles from './SubscribeForm.module.scss'
 import http from '@/api/common-configuration'
 import { useForm } from 'react-hook-form'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react'
+import { IStatus } from '../subscribeModal/SubscribeModal'
+import Image from 'next/image'
+import loading from '@/assets/svg/loading.svg'
 
 interface SignUpForm {
   email: string
@@ -12,19 +15,21 @@ interface SignUpForm {
 
 interface Props {
   handleClose: () => void
+  status: IStatus
+  setStatus: Dispatch<SetStateAction<IStatus>>
 }
 const SubscribeForm = (props: Props) => {
   const t = useTranslations('Modal')
   const t1 = useTranslations('')
   const [points, setPoints] = useState<string[]>([])
 
-  const handleCheck = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
     let newPoints = [...points]
-    e.target.checked 
-    ? 
-    newPoints = [...points, e.target.value]
-    :
-    newPoints = newPoints.filter(point => point !== e.target.value)
+    e.target.checked
+      ?
+      newPoints = [...points, e.target.value]
+      :
+      newPoints = newPoints.filter(point => point !== e.target.value)
     setPoints(newPoints)
   }
 
@@ -37,15 +42,15 @@ const SubscribeForm = (props: Props) => {
 
   const sendData = async () => {
     try {
+      props.setStatus({ status: 'loading' })
       const result = await http.post('/', {
         email: getValues('email'),
         points: points,
         language: t1('Lang')
       })
-      console.log(result)
-      props.handleClose()      
+      props.setStatus({ status: 'ok' })
     } catch (e) {
-      console.log(e)
+      props.setStatus({ status: 'error' })
     }
   }
   const emailPatternValidator = {
@@ -65,23 +70,30 @@ const SubscribeForm = (props: Props) => {
       <p>{t('Question')}</p>
       <div className={styles.form__checkList}>
         <label>
-          <input type='checkbox' value='search_room' onChange={(e) => handleCheck(e) } />
+          <input type='checkbox' value='search_room' onChange={(e) => handleCheck(e)} />
           <span>{t('Point1')}</span>
         </label>
         <label>
-          <input type='checkbox' value='rent_room' onChange={(e) => handleCheck(e) } />
+          <input type='checkbox' value='rent_room' onChange={(e) => handleCheck(e)} />
           <span>{t('Point2')}</span>
         </label>
         <label>
-          <input type='checkbox' value='search_people' onChange={(e) => handleCheck(e) } />
+          <input type='checkbox' value='search_people' onChange={(e) => handleCheck(e)} />
           <span>{t('Point3')}</span>
         </label>
         <label>
-          <input type='checkbox' value='manage_household' onChange={(e) => handleCheck(e) } />
+          <input type='checkbox' value='manage_household' onChange={(e) => handleCheck(e)} />
           <span>{t('Point4')}</span>
         </label>
       </div>
-      <Button id='subscribe' onClick={sendData} disabled={!isValid}>{t('Button')}</Button>
+      {props.status.status === 'error' &&
+        <p className={styles.form__error}>{t('Error')}</p>
+      }
+      <Button id='subscribe' onClick={sendData} disabled={!isValid || props.status.status === 'loading'}>
+        {props.status.status === 'loading' ?
+          <Image src={loading} alt='loading icon' width={16} height={16} className={styles.form__loading} /> :
+          t('Button')}
+      </Button>
     </>
   )
 }
--- src/components/blocks/subscribeModal/SubscribeModal.tsx
+++ src/components/blocks/subscribeModal/SubscribeModal.tsx
@@ -5,14 +5,20 @@ import Image from 'next/image'
 import styles from './SubscribeModal.module.scss'
 import logo from '@/assets/svg/logo.svg'
 import close from '@/assets/svg/closeWhite.svg'
+import ok from '@/assets/svg/ok.svg'
 import SubscribeForm from '../subscribeForm/SubscribeForm'
 import { useTranslations } from 'next-intl'
+import { useState } from 'react'
 
 interface Props {
   open: boolean,
   handleClose: () => void
 }
+export interface IStatus {
+  status: 'base' | 'loading' | 'ok' | 'error'
+}
 const SubscribeModal = (props: Props) => {
+  const [status, setStatus] = useState<IStatus>({ status: 'base' })
   const t = useTranslations('Modal')
   return (
     <div className={`${styles.modal} ${!props.open && styles.modal__hide}`} onClick={props.handleClose}>
@@ -22,16 +28,26 @@ const SubscribeModal = (props: Props) => {
             <Image src={close} alt='Close modal' width={32} height={32} />
           </button>
           <div className={styles.modal__heroBlock}>
-            <Image src={logo} alt='Roommate logo'  className={styles.modal__logo_image}/>
+            <Image src={logo} alt='Roommate logo' className={styles.modal__logo_image} />
             <h2 className={styles.modal__logo_text}>Roommate.host</h2>
           </div>
           <div className={styles.modal__content}>
             <div className={styles.modal__content_block}>
-              <h2 className={styles.modal__header}>{t('Header')}</h2>
-              <p className={styles.modal__text}>{t('Text')}</p>
-            </div>
-            <div className={styles.modal__content_block}>
-              <SubscribeForm handleClose={props.handleClose} />
+              {status.status !== 'ok' ?
+                <>
+                  <div className={styles.modal__content_block}>
+                    <h2 className={styles.modal__header}>{t('Header')}</h2>
+                    <p className={styles.modal__text}>{t('Text')}</p>
+                  </div>
+                  <SubscribeForm handleClose={props.handleClose} status={status} setStatus={setStatus} />
+                </>
+                :
+                <div className={styles.modal__ok}>
+                  <Image src={ok} alt='Subscription succesfull' />
+                  <p>{t('Ok')}</p>
+                  <Button onClick={props.handleClose}>Close</Button>
+                </div>
+              }
             </div>
           </div>
         </div>
