f9926636dd7948d8b9692a4f3a4cbc5fb7450470
wrongangel <wrongangel@gmail.com>
2023-07-10 18:18:05 +0300

    OpenCover setup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,19 +8,48 @@ import Household from '@/components/blocks/household/Household'
 import Prices from '@/components/blocks/prices/Prices'
 import styles from './page.module.scss'
 import PricesTriple from '@/components/blocks/pricesTriple/PricesTriple'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header />
-      <Hero />
-      <People />
-      <Brake />
-      <Place />
-      <Household />
-      <Brake second />
-      <PricesTriple />
-      <Footer />
-    </main>
+    <>
+      <NextSeo
+        title='Roommate'
+        description='An app for finding roommates'
+        canonical='https://roommate.host'
+        openGraph={{
+          url: 'https://roommate.host',
+          title: 'Roommate',
+          description: 'Find a room & a perfect roommate quick and easy',
+          images:[
+            {
+              url: '/openGraph_cover_800x600.png',
+              width: 800,
+              height: 600,
+              alt: 'Roommate.host',
+              type: 'image/png'
+            },
+            {
+              url: '/openGraph_cover_900x800.png',
+              width: 900,
+              height: 800,
+              alt: 'Roommate.host',
+              type: 'image/png'
+            }
+          ]
+        }}
+      />
+      <main className={styles.main}>
+        <Header />
+        <Hero />
+        <People />
+        <Brake />
+        <Place />
+        <Household />
+        <Brake second />
+        <PricesTriple />
+        <Footer />
+      </main>
+    </>
   )
 }
--- src/app/[locale]/tos/page.tsx
+++ src/app/[locale]/tos/page.tsx
@@ -2,15 +2,44 @@ import Footer from '@/components/blocks/footer/Footer'
 import Header from '@/components/blocks/header/Header'
 import TOS from '@/components/blocks/tos/tos'
 import styles from '../page.module.scss'
+import { NextSeo } from 'next-seo'
 
 interface Props { }
 const ToS = (props: Props) => {
   return (
-    <main className={styles.main}>
-      <Header />
-        <TOS />
-      <Footer />
-    </main>
+    <>
+      <NextSeo
+          title='Roommate'
+          description='Roommate.host terms and conditions'
+          canonical='https://roommate.host'
+          openGraph={{
+            url: 'https://roommate.host/tos',
+            title: 'Roommate',
+            description: 'Roommate.host terms and conditions',
+            images:[
+              {
+                url: '/openGraph_cover_800x600.png',
+                width: 800,
+                height: 600,
+                alt: 'Roommate.host',
+                type: 'image/png'
+              },
+              {
+                url: '/openGraph_cover_900x800.png',
+                width: 900,
+                height: 800,
+                alt: 'Roommate.host',
+                type: 'image/png'
+              }
+            ]
+          }}
+        />
+      <main className={styles.main}>
+        <Header />
+          <TOS />
+        <Footer />
+      </main>
+    </>
   )
 }
 export default ToS
